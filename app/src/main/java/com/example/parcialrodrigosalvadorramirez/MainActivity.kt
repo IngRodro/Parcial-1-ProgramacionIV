8cbca4c0b77cecca7eb6059c8bae6d287e4ec6d3
ElRodroUwU15 <rr0213032019@unab.edu.sv>
2021-09-10 11:21:46 -0600

    Ejercicios Parcial

--- app/src/main/java/com/example/parcialrodrigosalvadorramirez/MainActivity.kt
+++ app/src/main/java/com/example/parcialrodrigosalvadorramirez/MainActivity.kt
@@ -7,5 +7,60 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        EjercicioElseIf()
+        EjercicioWhen()
+        EjercicioBucle()
     }
+
+    private fun EjercicioElseIf(){
+
+        var color = "Azul"
+        if(color == "Celeste"){
+            println("El color seleccionado es $color")
+        }else if(color == "Verde"){
+            println("El color seleccionado es $color")}
+        else if(color == "Amarillo"){
+            println("El color seleccionado es $color")}
+        else if(color == "Azul"){
+            println("El color seleccionado es $color")}
+        else if(color == "Negro"){
+            println("El color seleccionado es $color")}
+        else{
+            println("El color no ha sido encontrado")}
+    }
+
+    private fun EjercicioWhen() {
+        var comida = "Hot Dog"
+
+        when(comida){
+            "Pupusas", "Elotes Locos", "Atol de Elote" ->{
+                println("El pais al que pertenece es El Salvador")  }
+            "Tacos", "Pozole" ->{
+                println("El pais al que pertenece es Mexico")  }
+            "Pizza", "Lasaña", "Ravioles" ->{
+                println("El pais al que pertenece es Italia")  }
+            "Hot Dog", "Hamburguesa" ->{
+                println("El pais al que pertenece es EEUU")  }
+            else ->{
+                println("No se conoce el pais de origen del Platillo")  }
+        }
+    }
+
+    private fun EjercicioBucle(){
+        var x = 15
+        println("Valores de 1 en 1")
+        for(valor in 0..x) {
+            println(valor)
+        }
+        println("Valores de 2 en 2")
+        for(valor in 0..x step 2){  println(valor)
+        }
+        println("Valores de 1 en 1 hacia abajo")
+        for(valor in x downTo 0){  println(valor)
+        }
+        println("Valores de 3 en 3 hacia abajo")
+        for(valor in x downTo 0 step 3){  println(valor)
+        }
+    }
+
 }
